89998cbfe1eac02d8d9b516b76e77422b1d257e3
baizita <baizita>
2017-12-08 13:11:36 +0800

    多张图片在一行时添加边框

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ fs.readFile(dirRoot +"/file/"+ fileName,"utf8",function(err,data){
 	}else{
 		data=data.replace(/<link.*?\/>/,"")//引用谷歌的样式表，会报错，所以去掉
 				 .replace(/@font-face.*?}/g,"")//引入字体，会报错，所以去掉
-				 .replace(/<p><img/g,"<p style='text-align:center'><img style='padding:5px;border:thin solid #ccc;border-radius:5px'")//原样式是图片居左，改为居中，且带有边框
+				 .replace(/<p><img/g,"<p style='text-align:center'><img")//原样式是图片居左，改为居中
+				 .replace(/<img/g,"<img style='padding:5px;border:thin solid #ccc;border-radius:5px'")//让图片带有边框
 				 .replace(/disabled='disabled'/g,"")//复选框都是禁用状态，改为可选状态
 				 .replace(/\n/g,"")//去掉所有的换行符，即压缩文件
 		fs.writeFile(dirRoot +"/dist/"+ fileName,data,"utf8",function(err,data){
@@ -20,6 +21,5 @@ fs.readFile(dirRoot +"/file/"+ fileName,"utf8",function(err,data){
 				console.log("file has been created successfully!")
 			}
 		})
-
 	}
 })
